057fde447ee85e4543311ce43784d2c2cceeb394
vilmar <vilmarmoreira2010@gmail.com>
2018-12-21 18:45:37 -0200

    add envia.php

--- envia.php
+++ envia.php
@@ -13,7 +13,7 @@ $host ="localhost";
 $user = "root";// NOME PADRAO DO XAMPP
 $pass = "";//XAMPP NAO USA SENHA
 $banco ="cadastro";
-$conexao = mysqli_connect($host,$user,$pass) or die(mysqli_error());
+$conexao = mysqli_connect($banco,$host,$user,$pass) or die(mysqli_error());
 mysqli_select_db($conexao,$banco) or die(mysqli_error());
 
  ?>
