b778d4799ac47caea0da1843c69592d9c26ca572
vilmar <vilmarmoreira2010@gmail.com>
2018-12-22 03:14:35 -0200

    new conection
    conexao estabelecida com banco de dados

--- envia.php
+++ envia.php
@@ -13,8 +13,7 @@ $host ="localhost";
 $user = "root";// NOME PADRAO DO XAMPP
 $pass = "";//XAMPP NAO USA SENHA
 $banco ="cadastro";
-$conexao = mysqli_connect($banco,$host,$user,$pass) or die(mysqli_error());
-mysqli_select_db($conexao,$banco) or die(mysqli_error());
+$conexao = mysqli_connect($host,$user,$pass,$banco) or die(mysqli_error());
 
  ?>
 
@@ -25,8 +24,7 @@ mysqli_select_db($conexao,$banco) or die(mysqli_error());
   $mensagem = $_POST["mensagem"];
 
 if(($_POST['email'] != "") and ($_POST['mensagem'] != "")and ($_POST['nome'] != "")){   // se formulario preenchido -nao vazio
-       $sql = mysqli_query($conexao,"INSERT INTO usuarios(nome,email,mensagem)VALUES('$nome,$email,$mensagem')");
-       mysqli_close($conexao);
+        mysqli_query($conexao," insert into usuario(nome,email,mensagem) values ('{$nome}','{$email}','{$mensagem}')");    
        echo("capturado");
        require("index.html");
      }
@@ -39,3 +37,4 @@ else{
 
 </body>
 </html>
+
